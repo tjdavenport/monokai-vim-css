3a6c2b30a4bd4db49ae7fc0b056bdc37e910a0aa
tjdavenport <tjdavenport2@gmail.com>
2022-09-30 02:15:07 +0000

    mess with possible invalid color change

--- scripts/input.js
+++ scripts/input.js
@@ -1,9 +1,21 @@
-function keyPressClassPoke(input, target, className) {
-  input.addEventListener('keydown', () => {
-    target.classList.add(className);
+const cOfSurgeKey = '--c-of-surge';
+
+function mvInput(root) {
+  const input = root.querySelector('input');
+  const surge = root.querySelector('.mv-input__animator')
+
+  input.addEventListener('invalid', () => {
+    console.log('invalid');
+    root.style.setProperty(cOfSurgeKey, `val(--c-pink)`);
   });
 
-  target.addEventListener('animationend', () => {
-    target.classList.remove(className);
+  input.addEventListener('keydown', () => {
+    //root.style.setProperty(cOfSurgeKey, initCOfSurge);
+    surge.classList.add('mv-input__animator--surge');
+    input.checkValidity();
+  });
+
+  surge.addEventListener('animationend', () => {
+    surge.classList.remove('mv-input__animator--surge');
   });
 }
