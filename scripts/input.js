b8846408c9e7df48955667435f30a38d37feeb4f
tjdavenport <tjdavenport2@gmail.com>
2022-12-10 19:04:56 -0500

    Revert "mess with possible invalid color change"
    This reverts commit 3a6c2b30a4bd4db49ae7fc0b056bdc37e910a0aa.

--- scripts/input.js
+++ scripts/input.js
@@ -1,21 +1,9 @@
-const cOfSurgeKey = '--c-of-surge';
-
-function mvInput(root) {
-  const input = root.querySelector('input');
-  const surge = root.querySelector('.mv-input__animator')
-
-  input.addEventListener('invalid', () => {
-    console.log('invalid');
-    root.style.setProperty(cOfSurgeKey, `val(--c-pink)`);
-  });
-
+function keyPressClassPoke(input, target, className) {
   input.addEventListener('keydown', () => {
-    //root.style.setProperty(cOfSurgeKey, initCOfSurge);
-    surge.classList.add('mv-input__animator--surge');
-    input.checkValidity();
+    target.classList.add(className);
   });
 
-  surge.addEventListener('animationend', () => {
-    surge.classList.remove('mv-input__animator--surge');
+  target.addEventListener('animationend', () => {
+    target.classList.remove(className);
   });
 }
